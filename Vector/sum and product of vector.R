c02f80916ae0c41795ac7b4fe480990fe3bfabe2
Abdullah-ICT-14 <mdabdullah01728@gmail.com>
2021-03-14 22:28:00 +0600

    sum(),prod(),cumsum(),cumprod(),mean(),var(),sd(),summary()

---
+++ Vector/sum and product of vector.R
@@ -0,0 +1,30 @@
+
+x <- c(10,1,3,5,15,2)
+x
+
+# sum using sum() function
+sum(x)
+
+# Product using prod function
+prod(x)
+
+# Cumulative sum using cumsum()
+cumsum(x)
+
+# Cumulative product
+cumprod(x)
+
+# Compute mean using mean() 
+mean(x)
+
+# Compute median using median()
+median(x)
+
+# Compute variance using var() 
+var(x)
+
+# Compute standard deviation using sd()
+sd(x)
+
+# To find the summary statistics
+summary(x)
