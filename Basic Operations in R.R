bf5d539bf6ed529990a205c7d90102d94616ec10
Abdullah-ICT-14 <mdabdullah01728@gmail.com>
2021-03-14 12:36:27 +0600

    Basic operations

---
+++ Basic Operations in R.R
@@ -0,0 +1,22 @@
+# Addition
+4+5
+
+# Subtraction
+4-5
+
+# Multiplication
+3*8
+
+# Division
+4/5
+
+# Power
+5^2
+
+
+# Equation 
+(7+3)*9-2
+
+
+# Inf(Infity) when anything is divided by zero
+7/0
