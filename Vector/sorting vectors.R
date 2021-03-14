93493009bd427857685870e344b0e137bdca772c
Abdullah-ICT-14 <mdabdullah01728@gmail.com>
2021-03-14 23:09:37 +0600

    Sorting

---
+++ Vector/sorting vectors.R
@@ -0,0 +1,20 @@
+x<-c(1,5,100,2,4)
+x
+
+# Sorting ascending order.
+sort(x)
+
+# Find the order
+order(x)
+
+# sort in descending order.
+sort(x,decreasing =TRUE)
+
+
+# Sort character vector
+char<-c('e','b','a','d','f')
+sort(char)
+
+# Sort logical vector
+logical_vector<-c(TRUE,FALSE,FALSE,TRUE,TRUE)
+sort(logical_vector)
