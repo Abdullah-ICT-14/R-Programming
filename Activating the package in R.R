8a0d16e1e88055bb21a111814f52521efe10abc6
Abdullah-ICT-14 <mdabdullah01728@gmail.com>
2021-03-14 12:08:44 +0600

    Activate the package in R

---
+++ Activating the package in R.R
@@ -0,0 +1,7 @@
+# Firtst we need to install the package from the packages section
+
+# To activate the package we need to write the name of the package within the library() or require().
+require(vcd)
+
+# Deactivate the package
+detach(package:vcd)
