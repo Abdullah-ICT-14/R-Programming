b5d4517c680b6f999b3583c8ae79abfcbddb04de
Abdullah-ICT-14 <mdabdullah01728@gmail.com>
2021-03-14 20:57:25 +0600

    Creating empty vectors

---
+++ Vector/creating empty vector.R
@@ -0,0 +1,17 @@
+
+# Using empty c() function
+empty_vector<-c()
+empty_vector
+typeof(empty_vector)
+
+# Using NULL value
+y <- NULL
+y
+
+# using vector() function
+z <- vector() # Create logical empty vector.
+z
+
+# Add value to empty vector
+z <- c(z,1,2)
+z
