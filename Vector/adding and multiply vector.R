f11e574702476b8ea25cf9351dc048de898550a4
Abdullah-ICT-14 <mdabdullah01728@gmail.com>
2021-03-17 16:28:03 +0600

    Arithmetic operation between two vectors

---
+++ Vector/adding and multiply vector.R
@@ -0,0 +1,40 @@
+# When the vectors are same length.
+
+x<-c(2,4,6,8)
+x
+y<-c(10,20,30,40)
+y
+
+z<- x+y
+z
+
+e <- y-x
+e
+
+f <- y/x
+f
+
+g <-y*x
+g
+
+# When the vectors are in different length
+# The shorter vector is recycled
+
+
+x<-c(2,4,6,8)
+x
+y<-c(10,20)
+y
+
+z<- x+y
+z
+
+e <- y-x
+e
+
+f <- y/x
+f
+
+g <-y*x
+g
+
