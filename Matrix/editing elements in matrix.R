65f61664cfab4f779710c1004d73fbba4ec59dcf
Abdullah-ICT-14 <mdabdullah01728@gmail.com>
2021-03-18 23:30:55 +0600

    Editing the value of matrix

---
+++ Matrix/editing elements in matrix.R
@@ -0,0 +1,15 @@
+
+m <- matrix(1:16,nrow=4)
+m
+
+# assign 100 to the element on row 3, column 4
+m[3,4]<-100
+m
+
+# assign the value 200 to the eight element
+m[8]<-200
+m
+
+# assign 0 to 2nd row
+m[2,] <-0
+m
