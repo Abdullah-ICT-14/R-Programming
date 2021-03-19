731fe51cbabd23a569d2b7c0f717808024ff2284
Abdullah-ICT-14 <mdabdullah01728@gmail.com>
2021-03-19 15:53:10 +0600

    Titanic Survival Predictions Using R Programs'

---
+++ Titanic/Titanic Survival Predictions Using R.R
@@ -0,0 +1,20 @@
+
+# This program predict the survival in the Titanic Disaster Using Decision Trees.
+# Decision Trees are powerfull tools in Machine learning
+
+# Importing the necessary library
+library(rpart) # Loading Decision Tree Package
+library(plyr) # means
+library(dplyr) # data cleaning
+library(stringr) # data cleaning
+library(naniar)  # missing data visualizations
+library(ggplot2) # Data visualization
+library(readr) # CSV file I/o, e.g. the read_csv function.
+library(rattle) # Visualize Decision trees
+library(rpart.plot) # visualize decision trees.
+library(caret) # model tunning
+library(caTools) # cross validation
+library(pROC) # ROC curves
+library(RColorBrewer) 
+library(Hmisc)
+library(kableExtra) # markdown tables
